f51282f05b648ac31399de974eeed7b69ba1a5ab
lucianobonzatto <37905647+lucianobonzatto@users.noreply.github.com>
2021-06-02 21:43:26 -0300

    Delete joy_control.sh

--- tello_control/shell_script/joy_control.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xterm  -e  "source ~/catkin_ws/devel/setup.bash;roslaunch tello_driver tello_node.launch" &
-sleep 1
-xterm  -e  "source ~/catkin_ws/devel/setup.bash;roslaunch tello_control joy_control.launch" &
