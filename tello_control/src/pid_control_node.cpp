61fb2195ff219c12140ad211bff74b5f9a78fcc8
lucianobonzatto <luciano.bj23@hotmail.com>
2022-04-08 19:57:22 -0300

    pid_control script and param

--- tello_control/src/pid_control_node.cpp
+++ tello_control/src/pid_control_node.cpp
@@ -2,6 +2,7 @@
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/Twist.h"
+#include "ar_track_alvar_msgs/AlvarMarkers.h"
 
 geometry_msgs::Point gotoPose;
 geometry_msgs::Pose telloPose;
@@ -11,15 +12,12 @@ void gotoCallback(const geometry_msgs::Pose::ConstPtr& msg){
   gotoPose.y = msg->position.y;
   gotoPose.z = msg->position.z;
 }
-void poseCallback(const geometry_msgs::Pose::ConstPtr& msg){
-  telloPose.position.x = msg->position.x;
-  telloPose.position.y = msg->position.y;
-  telloPose.position.y = msg->position.z;
-  telloPose.orientation.x = msg->orientation.x;
-  telloPose.orientation.y = msg->orientation.y;
-  telloPose.orientation.z = msg->orientation.z;
-  telloPose.orientation.w = msg->orientation.w;
-
+void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg){
+  if(!msg->markers.empty()){
+    telloPose.position.x = msg->markers[0].pose.pose.position.x;
+    telloPose.position.y = msg->markers[0].pose.pose.position.y;
+    telloPose.position.z = msg->markers[0].pose.pose.position.z;
+  }
 }
 
 int main(int argc, char *argv[])
@@ -39,15 +37,15 @@ int main(int argc, char *argv[])
 
   double time, last_time;
 
-  nh_param.param<double>("constante_P", cstP);
-  nh_param.param<double>("constante_I", cstI);
-  nh_param.param<double>("constante_D", cstD);
+  nh_param.getParam("constante_P", cstP);
+  nh_param.getParam("constante_I", cstI);
+  nh_param.getParam("constante_D", cstD);
 
   nh_param.param<double>("velocidade_maxima", maxVel);
 
   cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/tello/cmd_vel", 1);
   goto_sub = nh.subscribe<geometry_msgs::Pose>("/pid_control/goto", 1, &gotoCallback);
-  pose_sub = nh.subscribe<geometry_msgs::Pose>("/tello/odom/marker", 1, &poseCallback);
+  pose_sub = nh.subscribe<ar_track_alvar_msgs::AlvarMarkers>("/ar_pose_marker", 1, &poseCallback);
 
   geometry_msgs::Twist cmdVelMsg;
   cmdVelMsg.linear.x = 0;
@@ -59,26 +57,37 @@ int main(int argc, char *argv[])
 
   while(ros::ok()){
     ros::spinOnce();
-    ROS_INFO("X: %f, Y: %f, Z: %f", gotoPose.x, gotoPose.y, gotoPose.z);
 
     deltaX = telloPose.position.x - gotoPose.x;
     deltaY = telloPose.position.y - gotoPose.y;
     deltaZ = telloPose.position.z - gotoPose.z;
     time = ros::Time::now().toSec();
 
-    cmdVelMsg.linear.x = cstP*deltaX + cstI*(deltaX*(time-last_time)) + cstD*(deltaX-last_deltaX)/(time-last_time);
-    cmdVelMsg.linear.y = cstP*deltaY + cstI*(deltaY*(time-last_time)) + cstD*(deltaY-last_deltaY)/(time-last_time);
-    cmdVelMsg.linear.z = cstP*deltaZ + cstI*(deltaZ*(time-last_time)) + cstD*(deltaZ-last_deltaZ)/(time-last_time);
+    cmdVelMsg.linear.x = (-1)*cstP*deltaX + cstI*(deltaX*(time-last_time)) + cstD*(deltaX-last_deltaX)/(time-last_time);
+    cmdVelMsg.linear.y = (cstP*deltaY + cstI*(deltaY*(time-last_time)) + cstD*(deltaY-last_deltaY)/(time-last_time));
+    cmdVelMsg.linear.z = (cstP*deltaZ + cstI*(deltaZ*(time-last_time)) + cstD*(deltaZ-last_deltaZ)/(time-last_time));
     
     if (cmdVelMsg.linear.x > maxVel)
         cmdVelMsg.linear.x = maxVel;
+    if (cmdVelMsg.linear.x < -maxVel)
+        cmdVelMsg.linear.x = -maxVel;
 
     if (cmdVelMsg.linear.y > maxVel)
         cmdVelMsg.linear.y = maxVel;
+    if (cmdVelMsg.linear.y < -maxVel)
+        cmdVelMsg.linear.y = -maxVel;
 
     if (cmdVelMsg.linear.z > maxVel)
         cmdVelMsg.linear.z = maxVel;
-    
+    if (cmdVelMsg.linear.z < -maxVel)
+        cmdVelMsg.linear.z = -maxVel;
+
+    ROS_INFO("goto X: %f, goto Y: %f, goto Z: %f", gotoPose.x, gotoPose.y, gotoPose.z);
+    ROS_INFO("pose X: %f, pose Y: %f, pose Z: %f", telloPose.position.x, telloPose.position.y, telloPose.position.z);
+    ROS_INFO("cmdv X: %f, cmdv Y: %f, cmdv Z: %f", cmdVelMsg.linear.x, cmdVelMsg.linear.y, cmdVelMsg.linear.z);
+    ROS_INFO("cstP: %f, cstI: %f, cstD: %f", cstP, cstI, cstD);
+    ROS_INFO("\n\n");
+
     cmd_vel_pub.publish(cmdVelMsg);
     
     last_deltaX = deltaX;
