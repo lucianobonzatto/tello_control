b96f6507e6e06c8841d427970aefd579bfc7ad1c
lucianobonzatto <37905647+lucianobonzatto@users.noreply.github.com>
2022-04-29 12:45:14 -0300

    includ dungle flag

--- tello_driver/src/TelloPy/TelloPy/setup.py
+++ tello_driver/src/TelloPy/TelloPy/setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='tellopy',
 
-    version='0.7.0.dev0',
+    version='1.7.0.luciano',
 
     description='DJI Tello drone controller',
     url='https://github.com/hanyazou/TelloPy',
