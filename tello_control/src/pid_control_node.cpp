6efb4d34770603408c59a28517c0ea973cfa7bae
luciano bonzatto <luciano.bj23@hotmail.com>
2022-05-09 13:12:13 -0300

    pid flags and ar_tracker_alvar clone

--- tello_control/src/pid_control_node.cpp
+++ tello_control/src/pid_control_node.cpp
@@ -1,4 +1,6 @@
+#include <math.h>
 #include "ros/ros.h"
+#include "std_msgs/Bool.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/Twist.h"
@@ -6,8 +8,10 @@
 
 geometry_msgs::Point gotoPose;
 geometry_msgs::Pose telloPose;
+std_msgs::Bool running;
 
 ros::Publisher pose_pub;
+ros::Publisher cmd_vel_pub;
 int detect;
 
 void gotoCallback(const geometry_msgs::Pose::ConstPtr& msg){
@@ -42,6 +46,23 @@ void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg){
   }
   pose_pub.publish(out_msg);
 }
+void startCallback(const std_msgs::Bool::ConstPtr& msg){
+  //running->data = (int) msg->data;
+  if(running.data != msg->data){
+    if(msg->data == 0){
+      geometry_msgs::Twist cmdVelMsg;
+      cmdVelMsg.linear.x = 0;
+      cmdVelMsg.linear.y = 0;
+      cmdVelMsg.linear.z = 0;
+      cmdVelMsg.angular.x = 0;
+      cmdVelMsg.angular.y = 0;
+      cmdVelMsg.angular.z = 0;
+
+      cmd_vel_pub.publish(cmdVelMsg);
+    }
+    running.data = (int) msg->data;
+  }
+}
 
 int main(int argc, char *argv[])
 {
@@ -49,15 +70,17 @@ int main(int argc, char *argv[])
   ros::NodeHandle nh(""), nh_param("~");
   ros::Rate loop_rate(10);
 
-  ros::Publisher cmd_vel_pub;
+  ros::Publisher flag_pub;
   ros::Subscriber goto_sub;
   ros::Subscriber pose_sub;
+  ros::Subscriber start_sub;
 
   float deltaX, deltaY, deltaZ;
   float last_deltaX, last_deltaY, last_deltaZ;
   float cstP = 0.01, cstI = 0.01, cstD = 0.01;
   float cstP_eixoZ = 0.01, cstI_eixoZ = 0.01, cstD_eixoZ = 0.01;
   float maxVel = 1;
+  float dist_max = 0.1;
 
   double time, last_time;
 
@@ -69,21 +92,32 @@ int main(int argc, char *argv[])
   nh_param.getParam("constante_P_Z", cstP_eixoZ);
   nh_param.getParam("constante_I_Z", cstI_eixoZ);
   nh_param.getParam("constante_D_Z", cstD_eixoZ);
+  nh_param.getParam("DIST_MAX", dist_max);
 
   nh_param.param<double>("velocidade_maxima", maxVel);
 
   cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/tello/cmd_vel", 1);
   pose_pub = nh.advertise<geometry_msgs::Pose>("/pid_control/pose", 1);
+  flag_pub = nh.advertise<std_msgs::Bool>("/pid_control/flag", 1);
+
   goto_sub = nh.subscribe<geometry_msgs::Pose>("/pid_control/goto", 1, &gotoCallback);
   pose_sub = nh.subscribe<ar_track_alvar_msgs::AlvarMarkers>("/ar_pose_marker", 1, &poseCallback);
+  start_sub = nh.subscribe<std_msgs::Bool>("/pid_control/start", 1, &startCallback);
 
   geometry_msgs::Twist cmdVelMsg;
+  std_msgs::Bool flag_cheguei;
   cmdVelMsg.linear.x = 0;
   cmdVelMsg.linear.y = 0;
-  cmdVelMsg.linear.z = 2;
+  cmdVelMsg.linear.z = 0;
   cmdVelMsg.angular.x = 0;
   cmdVelMsg.angular.y = 0;
   cmdVelMsg.angular.z = 0;
+
+  gotoPose.x = 0;
+  gotoPose.y = 0;
+  gotoPose.z = 2;
+
+  running.data = 0;
 
   while(ros::ok()){
     ros::spinOnce();
@@ -100,11 +134,13 @@ int main(int argc, char *argv[])
     deltaZ = telloPose.position.z - gotoPose.z;
     time = ros::Time::now().toSec();
     
+    flag_cheguei.data = 0;
     if(detect == 1){
       cmdVelMsg.linear.x = (-1)*cstP*deltaX + cstI*(deltaX*(time-last_time)) + cstD*(deltaX-last_deltaX)/(time-last_time);
       cmdVelMsg.linear.y = (cstP*deltaY + cstI*(deltaY*(time-last_time)) + cstD*(deltaY-last_deltaY)/(time-last_time));
       cmdVelMsg.linear.z = (cstP_eixoZ*deltaZ + cstI_eixoZ*(deltaZ*(time-last_time)) + cstD_eixoZ*(deltaZ-last_deltaZ)/(time-last_time));
-    
+      flag_cheguei.data = 0;
+
       if (cmdVelMsg.linear.x > maxVel)
           cmdVelMsg.linear.x = maxVel;
       if (cmdVelMsg.linear.x < -maxVel)
@@ -119,6 +155,17 @@ int main(int argc, char *argv[])
           cmdVelMsg.linear.z = maxVel;
       if (cmdVelMsg.linear.z < -maxVel)
           cmdVelMsg.linear.z = -maxVel;
+      
+      if(abs(gotoPose.x - telloPose.position.x) < dist_max){
+        if(abs(gotoPose.y - telloPose.position.y) < dist_max){
+          if(abs(gotoPose.z - telloPose.position.z) < dist_max){
+            flag_cheguei.data = 1;
+            cmdVelMsg.linear.x = 0;
+            cmdVelMsg.linear.y = 0;
+            cmdVelMsg.linear.z = 0;
+          } 
+        }
+      }
     }
     else{
       cmdVelMsg.linear.x = 0;
@@ -126,15 +173,18 @@ int main(int argc, char *argv[])
       cmdVelMsg.linear.z = 0;
     }
     
-    
     ROS_INFO("goto X: %f, goto Y: %f, goto Z: %f", gotoPose.x, gotoPose.y, gotoPose.z);
     ROS_INFO("pose X: %f, pose Y: %f, pose Z: %f", telloPose.position.x, telloPose.position.y, telloPose.position.z);
     ROS_INFO("cmdv X: %f, cmdv Y: %f, cmdv Z: %f", cmdVelMsg.linear.x, cmdVelMsg.linear.y, cmdVelMsg.linear.z);
     ROS_INFO("cstP: %f, cstI: %f, cstD: %f", cstP, cstI, cstD);
     ROS_INFO("cstPz: %f, cstIz: %f, cstDz: %f", cstP_eixoZ, cstI_eixoZ, cstD_eixoZ);
+    ROS_INFO("cheguei: %i, running: %i", flag_cheguei.data, running.data);
     ROS_INFO("\n\n");
 
-    cmd_vel_pub.publish(cmdVelMsg);
+    if(running.data == 1){
+      cmd_vel_pub.publish(cmdVelMsg);
+      flag_pub.publish(flag_cheguei);
+    }
     
     last_deltaX = deltaX;
     last_deltaY = deltaY;
